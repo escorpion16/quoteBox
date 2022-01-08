3b84a9bbfd11df9d3b94cf41b7d1d2b1808f3fc1
Dennis <denizponce16@gmail.com>
2022-01-08 11:31:48 -0500

    finished project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import './App.css';
+import QuoteBox from './components/QuoteBox';
+import quotes from './quotes.json'
 
 function App() {
+
+  const colors = ["#845EC2","#2C73D2","#0089BA","#008E9B","#008F7A","#C34A36","#FFC75F"]
+  const [background, setBackground] = useState(colors[getQuoteRandom(colors.length)])
+  const [quote, setQuote] = useState(quotes[getQuoteRandom(quotes.length)])
+
+  const handleQuote = () => {
+    setQuote(quotes[getQuoteRandom(quotes.length)])
+    setBackground(colors[getQuoteRandom(colors.length)])
+  }
+    
+
   return (
-    <div className="App">
-      
+    <div className="App" style={{background}}>
+      <QuoteBox quote={quote.quote} author={quote.author} handleQuote={handleQuote} color={background} />
     </div>
   );
 }
+
+const getQuoteRandom = max => Math.floor(Math.random() * max) 
 
 export default App;
--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import  '../styles/quoteBox.css'
 
-const QuoteBox = () => {
+const QuoteBox = ({ quote, author, handleQuote, color }) => {
     return (
-        <div>
-            
+        <div className='cardQuote'>
+            <div className='phraseContainer'>
+                <i style={{color}} className="fas fa-quote-left"></i>
+                <p style={{color}} className='phrase'>{ quote }</p>
+            </div>
+            <div className='content'>
+                <p style={{color}}>{ author }</p>
+                <div className='btnChangeQuote' >
+                    <button style={{color}} onClick={handleQuote}> <i class="fas fa-arrow-circle-right"></i> </button>
+                </div>
+            </div>
         </div>
     )
 }
